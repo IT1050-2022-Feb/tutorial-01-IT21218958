143c26487d7bec375283e6582b9d58023846a62f
IT21218958 <99387339+IT21218958@users.noreply.github.com>
2022-02-13 10:47:06 +0000

    Tute 01 - Ex. 01

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,19 @@
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+  int mark1, mark2;
+  float average;
+
+  printf("Enter the marks of subject 1: ");
+  scanf("%d", &mark1);
+  printf("Enter the marks of subject 2: ");
+  scanf("%d",& mark2);
   
+  average = (mark1 + mark2) / 2.0;
+
+  printf("Average = %.2f", average);
+
   return 0;
 }
-
